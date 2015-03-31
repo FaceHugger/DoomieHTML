94ff7e9fc30ba5a254744c8e206e51b9777383f0
Marien <mir3nsita92@gmail.com>
2015-03-30 22:18:44 -0430

    first testing of inputs

---
+++ DOOM_tree/testing.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+
+ *
+ *  Created on: 30/3/2015
+ *      Author: Marien
+ */
+
+#include "DOM_Tree.h"
+#include "Node.h"
+#include "Element.h"
+#include <string>
+
+
+int main()
+{
+	DOM_Tree aTree;
+	Node n1;
+	string example;
+	Element elem;
+
+//	elem =
+
+	//n1 = new Node(elem);
+
+	cin >> example;
+}
+
+
